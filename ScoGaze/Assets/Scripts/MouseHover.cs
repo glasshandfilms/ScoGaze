3eb7485111aef82fb120b9edd1eeaed976c8c172
glasshandfilms <brandon@glasshandstudios.com>
2019-05-11 17:19:59 -0400

    initial push and hover script

---
+++ ScoGaze/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+
+    [SerializeField] private float duration = 1f;
+    private float timer = 0f;
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        
+        
+    }
+}
