11603eb1a90322fcaba854c1f9a184db0282f43e
glasshandfilms <brandon@glasshandstudios.com>
2019-05-11 23:04:39 -0400

    finished

--- ScoGaze/Assets/Scripts/MouseHover.cs
+++ ScoGaze/Assets/Scripts/MouseHover.cs
@@ -1,25 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MouseHover : MonoBehaviour
 {
 
-    [SerializeField] private float duration = 1f;
-    private float timer = 0f;
-
+    //[SerializeField] private float duration = 10f;
+    [SerializeField] private float redTimer = 0f;
+    [SerializeField] private float blueTimer = 0f;
+    [SerializeField] private GameObject BlueGameObject;
+    [SerializeField] private GameObject RedGameObject;
+    [SerializeField] private GameObject JokeGameObject;
+    
+    Ray ray;
+    RaycastHit hit;
+    
     
 
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Red")
+        {
+            redTimer += Time.deltaTime;
+            RedGameObject.GetComponent<TextMeshProUGUI>().text = hit.collider.name + "Timer: " + System.Math.Round(redTimer, 2);
+        }
         
+
+        else if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Blue")
+        {
+            blueTimer += Time.deltaTime;
+            BlueGameObject.GetComponent<TextMeshProUGUI>().text = hit.collider.name + "Timer: " + System.Math.Round(blueTimer, 2);
+        }
+        
+
+        if (redTimer >= 10f)
+        {
+            JokeGameObject.GetComponent<TextMeshProUGUI>().text = "You stared at the Red Cube for 10 seconds!";
+        }
+        else if (blueTimer >= 10f)
+        {
+            JokeGameObject.GetComponent<TextMeshProUGUI>().text = "You stared at the Blue Cube for 10 seconds!";
+        }
         
     }
+
+    
 }
